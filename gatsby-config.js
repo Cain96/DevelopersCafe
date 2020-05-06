b640aaa265cd4b8e542f5d07813a278e1cb6ebc7
Kenta <tk.777.k@gmail.com>
2020-05-06 20:55:20 +0900

    add setting anchor rss feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+const path = require(`path`);
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Typescript Starter`,
@@ -7,16 +9,23 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-typescript`,
     {
-      resolve: 'gatsby-plugin-purgecss',
+      resolve: `gatsby-plugin-purgecss`,
       options: {
         content: [
-          require("path").join(
+          path.join(
             process.cwd(),
             "src/**/!(*.d).{js,jsx,ts,tsx,md,mdx}"
           ),
         ],
         printRejected: true,
         develop: false,
+      },
+    },
+    {
+      resolve: `gatsby-source-rss-feed`,
+      options: {
+        url: `https://anchor.fm/s/8585ef0/podcast/rss`,
+        name: `AnchorPodCast`,
       },
     },
   ],
