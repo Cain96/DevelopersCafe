c645b16556aa6daebaadf8808c33f44fb9781148
Kenta <tk.777.k@gmail.com>
2020-05-07 02:37:30 +0900

    Add SEO component

---
+++ config/site.js
@@ -0,0 +1,17 @@
+module.exports = {
+  pathPrefix: '/',
+  title: 'Developers Cafe',
+  titleAll: 'Developers Cafe',
+  description: 'Welcome to Developers Cafe website',
+  url: 'https://developers-cafe.tech',
+  siteUrl: 'https://developers-cafe.tech/',
+  siteLanguage: 'ja',
+  logo: 'static/logo/logo.png',
+  banner: 'static/logo/banner.png',
+
+  favicon: 'static/logo/favicon.png',
+  shortName: 'DevCafe',
+  author: 'Developers Cafe',
+  themeColor: '#ffffff',
+  backgroundColor: '#ffffff',
+};
