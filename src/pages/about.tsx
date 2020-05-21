8f8389c520be9b9865fce45e7fcef923962d0f2e
Tirol_JPN <Tirol.JPN@gmail.com>
2020-05-21 10:01:03 +0900

    Delete unnecessary html files and elements

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -10,8 +10,6 @@ const SecondPage: FC<Props> = ({ location }) => {
   return (
     <Layout location={location}>
       <div>
-        <h1>Hi from the second page</h1>
-        <p>Welcome to page 2</p>
         <Link to="/">Go back to the homepage</Link>
       </div>
     </Layout>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,12 +20,7 @@ const IndexPage: FC<Props> = ({ location, data }) => {
   return (
     <Layout location={location}>
       <div>
-        <h1>Hi people</h1>
-        <p>
-          Welcome to your new <strong>{data.site.siteMetadata.title}</strong> site.
-        </p>
-        <p>Now go build something great.</p>
-        <Link to="/page-2/">Go to page 2</Link>
+        <Link to="/about/">AboutUs</Link>
       </div>
     </Layout>
   );
